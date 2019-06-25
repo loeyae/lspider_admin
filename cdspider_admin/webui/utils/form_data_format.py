ba494475c4af6fad20165828af7b4f78bd7c1cc2
zhangyi <zhangyi07@beyondsoft.com>
2019-06-25 21:27:03 +0800

    fix bug

--- cdspider_admin/webui/utils/form_data_format.py
+++ cdspider_admin/webui/utils/form_data_format.py
@@ -467,6 +467,7 @@ def build_form_data(data):
             for _k, val in parser['item'].items():
                 data['parse']['other'][_k] = val
         elif parser:
+            print(parser)
             for _k, val in parser.items():
                 data['parse']['other'][_k] = val
     else:
