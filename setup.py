0c81fcf0d5593cb44cebf3e45ae8783e1c0703d0
ZhangYi <loeyae@gmail.com>
2021-04-17 20:35:54 +0800

    增加依赖

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         'flask>=0.12.2',
         'flask_login>=0.4.1',
         'WsgiDAV==2.3.0',
+        'Werkzeug==0.16.1',
         'cdspider>=0.1',
         'cdspider_extra>=0.1',
         'cdspider_bbs>=0.1',
